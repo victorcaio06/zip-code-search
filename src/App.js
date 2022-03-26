c18a6e95aeee5fc34087ec5c72dbb460432eb7f5
Victor Caio <kaioaraujo426@gmail.com>
2022-03-25 21:58:29 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import './app.css' 
+import { RiSearch2Line } from "react-icons/ri";
+import { useState } from 'react';
+import API from './services/api';
 
 function App() {
+
+    const [input, setInput] = useState('');
+    const [output, setOutput] = useState({});
+
+    async function handleSearch() {
+        //01001000/json/
+
+        if(input === ''){
+            alert("Preencha o campo do CEP");
+            return;
+        }
+
+        try{
+            const RESPONSE = await API.get(`${input}/json`);
+            setOutput(RESPONSE.data);
+            setInput("");
+        }
+        catch{
+            alert("OPS!! Algo deu errado, digite o CEP novamente");
+            setInput('');
+        }
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1 className="title">Buscador de CEP</h1>
+        <section className="section-input">
+            <input 
+                type="text" 
+                placeholder="Digite o CEP"
+                value={input}
+                onChange={(event) => setInput(event.target.value)}
+            />
+            <button className="btn-search" onClick={handleSearch}>
+                <RiSearch2Line size={28} color="#FFF"/>
+            </button>
+        </section>
+        {Object.keys(output).length > 0 && (
+            <main className="content">
+                <h2>CEP: {output.cep}</h2>
+                <span>Logradouro: {output.logradouro}</span>
+                <span>Complemento: {output.complemento}</span>
+                <span>Bairro: {output.bairro}</span>
+                <span>Localidade: {output.localidade} - {output.uf}</span>
+             </main>
+        )}   
     </div>
   );
 }
